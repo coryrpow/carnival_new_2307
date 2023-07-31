9ae972960b9dcab23dc08b74fb56c56c9c92bd7c
Cory Powell <coryrpowell@gmail.com>
2023-07-31 12:12:29 -0400

    add ride class and attributes with tests

---
+++ lib/ride.rb
@@ -0,0 +1,15 @@
+class Ride
+  attr_reader :name,
+              :min_height,
+              :admission_fee,
+              :excitement,
+              :total_revenue
+  def initialize(data)
+    @name = data[:name]
+    @min_height = data[:min_height]
+    @admission_fee = data[:admission_fee]
+    @excitement = data[:excitement]
+    @total_revenue = 0
+  end
+
+end
