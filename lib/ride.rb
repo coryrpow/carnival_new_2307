cf9401ecc9f3f31f5a3c0aa5a1ebad6ff80c654a
Cory Powell <coryrpowell@gmail.com>
2023-07-31 13:32:22 -0400

    add additional funcitonality/tests to board_rider method

--- lib/ride.rb
+++ lib/ride.rb
@@ -1,15 +1,31 @@
+require './lib/visitor'
+
 class Ride
   attr_reader :name,
               :min_height,
               :admission_fee,
               :excitement,
-              :total_revenue
+              :total_revenue,
+              :rider_log
+              
   def initialize(data)
     @name = data[:name]
     @min_height = data[:min_height]
     @admission_fee = data[:admission_fee]
     @excitement = data[:excitement]
     @total_revenue = 0
+    @rider_log = {}
   end
 
+  def board_rider(visitor)
+    if visitor.preferences.include?@excitement
+      if visitor.tall_enough?(@min_height)
+        if visitor.spending_money >= @admission_fee
+          visitor.spending_money -= @admission_fee
+          @total_revenue += @admission_fee
+          @rider_log[visitor.name] = visitor
+        end
+      end
+    end
+  end
 end
--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -37,4 +37,31 @@ RSpec.describe Visitor do
     end
   end
 
+  describe "#board_rider" do
+    it "can add riders based and check the rider's preferences and charge admission_fee" do
+      @visitor1.add_preference(:gentle)
+      @visitor2.add_preference(:gentle)
+      @ride1.board_rider(@visitor1)
+      @ride1.board_rider(@visitor2)
+      @ride1.board_rider(@visitor1)
+      
+      expect(@visitor1.spending_money).to eq(8)
+      expect(@visitor2.spending_money).to eq(4)
+      expect(@ride1.total_revenue).to eq(3)
+
+      @visitor2.add_preference(:gentle)
+      @visitor2.add_preference(:thrilling)
+      @visitor3.add_preference(:thrilling)
+
+      @ride3.board_rider(@visitor1)
+      @ride3.board_rider(@visitor2)
+      @ride3.board_rider(@visitor3)
+      
+      expect(@visitor1.spending_money).to eq(8)
+      expect(@visitor2.spending_money).to eq(4)
+      expect(@visitor3.spending_money).to eq(13)
+      expect(@ride3.rider_log).to eq({ "Penny" => @visitor3 })
+      expect(@ride3.total_revenue).to eq(2)
+    end
+  end
 end
